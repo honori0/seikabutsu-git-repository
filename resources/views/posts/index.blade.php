c7ad394c286a775b3ed58b7b3b9be9176a00b3d7
honori0 <honori.tsuyama@gmail.com>
2023-01-16 17:42:06 +0900

    追加：投稿の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,6 +20,11 @@
                     </h3>
                     <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
                     <p>{{$post->created_at}}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                    </form>
                 </div>
             @endforeach
         </div>
@@ -27,4 +32,13 @@
             {{ $posts->links() }}
         </div>
     </body>
+    <script>
+    function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+</script>
 </html>
