49854c8e0bedac3d8fdbcc3358fa9c763e29c3ff
honori0 <honori.tsuyama@gmail.com>
2023-02-15 23:01:05 +0900

    追加：tailwind

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,11 +26,25 @@
                 <p>update: {{ $post->updated_at }}</p>
             </div>
         </div>
-        <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
+        <a class="bg-gray-500 hover:bg-gray-400 text-white rounded px-4 py-2" href="/posts/{{ $post->id }}/edit">edit</a>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" class="bg-gray-500 hover:bg-gray-400 text-white rounded px-4 py-2" onclick="deletePost({{ $post->id }})">delete</button> 
+                    </form>
         <div class="footer">
             <a href="/">戻る</a>
         </div>
         {{ Auth::user()->name }}
+    <script>
+    function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+    </script>
     </body>
     </x-app-layout>
 </html>
